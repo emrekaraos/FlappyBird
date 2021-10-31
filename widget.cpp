6385d99de7693d318bf5f04359aebac9b1678e2b
Emre <emrekaraosmanoglu1@gmail.com>
2021-10-31 03:36:39 +0300

    add graphicsView item

--- widget.cpp
+++ widget.cpp
@@ -15,6 +15,15 @@ Widget::Widget(QWidget *parent)
     QGraphicsPixmapItem * pixItem = new QGraphicsPixmapItem(QPixmap(":/sky.png"));
 
     scene->addItem(pixItem);
+    pixItem ->setPos(QPointF(0,0) - QPointF(pixItem->boundingRect().width()/2,
+                                            pixItem->boundingRect().height()/2));
+
+
+    scene->addLine(-400, 0, 400, 0, QPen(Qt::blue));
+    scene->addLine(0, -400, 0, 400, QPen(Qt::blue));
+
+
+
 
     ui->graphicsView->setScene(scene);
 }
